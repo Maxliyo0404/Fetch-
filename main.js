30a76185fded7793fced026c6a74f0d1dd84e559
Mahliyo <gasandigital13@gmail.com>
2026-04-13 20:53:16 +0500

    json

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function  render (users){
        <div class="card">
            <img class="rasm"  src="https://picsum.photos/200/300?random=id${el.id} " alt="rasm">
           <h2>${el.name}</h2>
-          <p>${el. }</p>
+          <p>${el.telefon }</p>
           <a href="#">${el.email}</a>
     </div> 
         `).join("");
